275d57b7fda9611d2c78dfd77ba1baf45b7b848f
Remo Zumsteg <remolueoend@users.noreply.github.com>
2018-12-26 02:54:28 +0100

    #10 setup styled components incl. global styles

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,8 +3,11 @@ import { connect } from "react-redux"
 import { lifecycle } from "recompose"
 import { AppState } from "root-reducer"
 import { BookmarksNode, fetchBookmarks } from "state/bookmarks"
+import styled from "styled-components"
 
 export interface HomeProps {
+  style?: {}
+  className?: string
   loading: boolean
   rootNode: BookmarksNode | undefined
   fetchBookmarks: () => void
@@ -29,20 +32,32 @@ const BookmarksRoot: React.SFC<{ nodes: BookmarksNode[] }> = ({ nodes }) => (
   </ul>
 )
 
-const Home: React.SFC<HomeProps> = ({ loading, rootNode }) =>
-  loading && !rootNode ? (
-    <span>loading...</span>
-  ) : !rootNode ? (
-    <span>no bookmarks...</span>
-  ) : (
-    <BookmarksRoot nodes={rootNode.children || []} />
-  )
+const Home: React.SFC<HomeProps> = ({
+  loading,
+  rootNode,
+  style,
+  className,
+}) => (
+  <div className={className} style={style}>
+    {loading && !rootNode ? (
+      <span>loading...</span>
+    ) : !rootNode ? (
+      <span>no bookmarks...</span>
+    ) : (
+      <BookmarksRoot nodes={rootNode.children || []} />
+    )}
+  </div>
+)
+
+const StyledHome = styled(Home)`
+  color: darkgray;
+`
 
 const HomeWithLifecicle = lifecycle<HomeProps, never>({
   componentDidMount() {
     this.props.fetchBookmarks()
   },
-})(Home)
+})(StyledHome)
 
 const HomeWithState = connect(
   (state: AppState) => ({
--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -2,7 +2,15 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { createUIStore } from "redux-webext"
+import { createGlobalStyle } from "styled-components"
 import Home from "../../components/home"
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url(${browser.extension.getURL("assets/Nunito-Regular.ttf")});
+    font-family: 'Nunito', sans-serif;
+  }
+`
 
 async function initApp() {
   const store = await createUIStore<{}>()
@@ -12,6 +20,7 @@ async function initApp() {
 
   ReactDOM.render(
     <Provider store={store}>
+      <GlobalStyles />
       <Home />
     </Provider>,
     mountNode,
